8a86c91abb1d4d163e3d1c035629b5fd2d70bfba
Gerardo Marx <gmarx_cc@itmorelia.edu.mx>
2023-09-01 02:57:25 +0200

    timer and loop

--- temp.sh
+++ temp.sh
@@ -24,3 +24,14 @@ echo "Temperature is"
 echo "$(( $temp / 1000 ))"
 echo "$temp * 0.001 " | bc
 
+sum=0
+for i in {1..4}
+do
+	echo "$i"
+	sleep 1s
+	temp=$(printTemp)
+	sum=$(( temp+sum ))
+done
+
+echo "$sum / 4  * 0.001" | bc
+
