f2baca3c756ffb6bd8a26467102e3a72262df497
Gerardo Marx <gmarx_cc@itmorelia.edu.mx>
2023-09-01 02:24:58 +0200

    converting

--- temp.sh
+++ temp.sh
@@ -2,9 +2,25 @@
 
 TEMP_PATH=/sys/class/thermal/thermal_zone0/temp
 
+function welcome
+{
+	echo "Welcome $USER, program started"
+}
+
+
+
 function printTemp
 {
 	cat $TEMP_PATH
 }
 
-printTemp
+
+# Comments
+# init of main code
+
+welcome
+temp=$(printTemp)
+echo "Temperature is"  
+echo "$(( $temp / 1000 ))"
+echo "$temp * 0.001 " | bc
+
